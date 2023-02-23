d8c3ced3e8bd3577f6b9f0af187f8c2f8e5eadba
SABAH <SABAH@DESKTOP-E30TBPJ>
2023-02-23 14:53:24 +0300

    oop not

---
+++ OopGiris/Giris.cs
@@ -0,0 +1,57 @@
+﻿/* "Access Modifiers - Erişim Belirleyiciler"
+ * public => her yerden erişilebilir 
+ * private => sadece tanımlandığı sınıf içerisinden erişilir.Sınıfa özel
+ * internal => sadece tanımlandığı proje içerisinden erişilebilir
+ * procted -
+ * procted internal-
+
+ **********************************************************************
+
+ * "Default Access Modifiers - Varsayılan Erişim Belirleyiciler"
+ *class, struct, interface , enum- intern
+ *fieald , property , method, event - private
+
+ ***********************************************************************
+
+ * "Sınıf Öğeleri"
+ *Constructor => Yapıcı Metot
+ *Her statik olmayan sınıf içerisinde yazmasakta bir adet yapıcı metot vardır
+ *Yapıcı metotlr sınıfın örneği (instance) alındığında ilk çalışan metottur.
+ *Yapıcı metotlarda methot overloading yapılabilir
+
+ ***********************************************************************
+
+ * "Fields - Alanlar"
+ *Sınıf içerisinde tanımlanan değişkenlere field/alan denir
+ *Fieldlar sınıfın örneği alındığında bellekte yer kaplarlar
+
+ ************************************************************************
+
+ * "Properties - Özellikler"
+ *Fieldları encapsulation yapmak için kullanılır
+ *Readonly properties = sadece get'i olan
+ *Full properties = get + set
+
+ ************************************************************************
+
+ * "Events - Olaylar"
+  
+ ***********************************************************************
+ 
+ * "Operatör Ovarloading - Operatör tanımlama"
+ * Sınıf içerisinde tanımlanan operatörlerin davranışlarını değiştirmek için kullanılır
+  
+ ***********************************************************************
+ 
+ * "Static "
+ * Static fiealdlar bellekte sadece bir kere yer kaplarlar.
+ * Ram'de hazır bekledikleri için hızlı çalışırlar
+
+*/
+
+namespace OopGiris
+{
+    internal class Giris
+    {
+    }
+}
