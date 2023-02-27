d3375b2ec0113504b6a4f964c4c1f4851d7c8315
SABAH <SABAH@DESKTOP-E30TBPJ>
2023-02-27 12:04:55 +0300

    Form class ekleme

--- WFormGiris/Form1.Designer.cs
+++ WFormGiris/Form1.Designer.cs
@@ -29,32 +29,58 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.lblKullaniciAdi = new System.Windows.Forms.Label();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(349, 103);
+            this.button1.Location = new System.Drawing.Point(404, 363);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Giriş";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblKullaniciAdi
+            // 
+            this.lblKullaniciAdi.AutoSize = true;
+            this.lblKullaniciAdi.Location = new System.Drawing.Point(37, 43);
+            this.lblKullaniciAdi.Name = "lblKullaniciAdi";
+            this.lblKullaniciAdi.Size = new System.Drawing.Size(79, 15);
+            this.lblKullaniciAdi.TabIndex = 1;
+            this.lblKullaniciAdi.Text = "Kullanıcı Adı :";
+            this.lblKullaniciAdi.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(140, 40);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(100, 23);
+            this.txtKullaniciAdi.TabIndex = 2;
+            this.txtKullaniciAdi.TextChanged += new System.EventHandler(this.txtKullaniciAdi_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(559, 450);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.lblKullaniciAdi);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Giriş";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button button1;
+        private Label lblKullaniciAdi;
+        private TextBox txtKullaniciAdi;
     }
 }
--- WFormGiris/Form1.cs
+++ WFormGiris/Form1.cs
@@ -1,5 +1,6 @@
 namespace WFormGiris
 {
+    //form týkla --> view--> taborder ->> imlecin hangi sýra ile gittiðini belirleyebiliriz
     public partial class Form1 : Form
     {
         public Form1()
@@ -8,6 +9,21 @@ namespace WFormGiris
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtKullaniciAdi_TextChanged(object sender, EventArgs e)
         {
 
         }
